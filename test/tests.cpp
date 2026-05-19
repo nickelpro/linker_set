3121fb54c4a507af4a80bfdc4740fe333f05c5df
Vito Gamberini <vito@gamberini.email>
2026-05-19 14:02:34 -0400

    test: code golfing

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <array>
 #include <cstddef>
+#include <functional>
 #include <span>
 
 #include <catch2/catch_test_macros.hpp>
@@ -8,28 +9,26 @@
 
 #include "util.hpp"
 
+namespace rg = std::ranges;
+
 auto proj = [](auto p) { return p ? *p : 0; };
 
 template <typename T>
-bool check_linker_set_size(std::span<T const* const> sp, std::size_t sz) {
-  if(sp.size() == sz)
-    return true;
-  return std::ranges::count_if(sp, [](auto i) { return i; }) ==
-      static_cast<long long>(sz);
+bool check_linker_set_size(std::span<T const* const> sp, std::ptrdiff_t sz) {
+  return std::ssize(sp) == sz || rg::count_if(sp, std::identity {}) == sz;
 }
 
 template <typename T, std::size_t N>
 bool check_linker_set_contains(std::span<T const* const> sp,
     std::array<T, N> arr) {
-  return std::ranges::all_of(arr,
-      [&](auto& el) { return std::ranges::contains(sp, el, proj); });
+  return rg::all_of(arr, [&](auto& el) { return rg::contains(sp, el, proj); });
 }
 
 TEST_CASE("LINKER_SET_ADD_UNIQUE") {
   auto sp {LINKER_SET_SPAN(add_unique)};
   REQUIRE(check_linker_set_size(sp, 4));
   REQUIRE(check_linker_set_contains(sp, std::array {1, 2, 3}));
-  REQUIRE(std::ranges::count_if(sp, [](auto i) { return i == 3; }, proj) == 2);
+  REQUIRE(rg::count_if(sp, [](auto i) { return i == 3; }, proj) == 2);
 }
 
 TEST_CASE("LINKER_SET_ADD") {
